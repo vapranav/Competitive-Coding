0499bd1edabf639db26c7fc0dc128c8279fab9d5
Pranav <50168375+vapranav@users.noreply.github.com>
2021-10-03 09:37:06 +0530

    Time: 52 ms (21.07%), Space: 30 MB (11.72%) - LeetHub

--- permutation-in-string/permutation-in-string.cpp
+++ permutation-in-string/permutation-in-string.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int s2Len = s2.length();
-        int s1Len = s1.length(); 
-        if(s2Len<s1Len)
+        if(s1.length() > s2.length())
             return false;
-        //int index;
-        vector<int> freq1(26, 0);
-        vector<int> freq2(26, 0);
-        for(int i = 0; i<s1Len; i++) {
-            freq1[s1[i] - 'a']++;
-            freq2[s2[i] - 'a']++;
+        vector<int> s1map(26, 0);
+        vector<int> s2map(26, 0);
+        
+        for(int i = 0; i<s1.length(); i++) {
+            s1map[s1[i] - 'a']++;
+            s2map[s2[i] - 'a']++;
         }
         
-        for(int i = 0; i<s2Len-s1Len; i++) {
-            if(matches(freq1, freq2)) {
+        for(int i = 0; i<s2.length() - s1.length(); i++) {
+            if(permutation(s1map, s2map))
                 return true;
+            else {
+                s2map[s2[i+s1.length()] - 'a']++;
+                s2map[s2[i] - 'a']--;
             }
-            freq2[s2[i] - 'a']--;
-            freq2[s2[i+s1Len] - 'a']++;
         }
         
-        return matches(freq1, freq2);
+        return permutation(s1map, s2map);
     }
-        
-        bool matches(vector<int> freq1, vector<int> freq2) {
-            for(int i = 0; i<26; i++) {
-                if(freq1[i] != freq2[i])
-                    return false;
-            }
-            return true;
+    
+    bool permutation(vector<int> s1, vector<int> s2) {
+        for(int i = 0; i<26; i++) {
+            if(s1[i] != s2[i])
+                return false;
         }
+        return true;
+    }
 };
